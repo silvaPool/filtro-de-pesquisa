44d8aeccdbf839967794ee933a9d152da0864e6c
silvaPool <silva.friends22@gmail.com>
2025-02-26 08:05:25 -0300

    renderizando a lista de itens

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
 import { useState } from "react";
 
+const users = [
+  { firstName: "John", id: 1 },
+  { firstName: "Emily", id: 2 },
+  { firstName: "Michael", id: 3 },
+  { firstName: "Sarah", id: 4 },
+  { firstName: "David", id: 5 },
+  { firstName: "Jessica", id: 6 },
+  { firstName: "Daniel", id: 7 },
+  { firstName: "Olivia", id: 8 },
+  { firstName: "Matthew", id: 9 },
+  { firstName: "Sophia", id: 10 },
+];
+
 function App() {
   const [searchItem, setSearchItem] = useState("");
 
@@ -9,14 +22,19 @@ function App() {
   };
 
   return (
-    <div>
+    <>
       <input
         type="text"
         value={searchItem}
         onChange={handleInputChange}
         placeholder="Type to search"
       />
-    </div>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.firstName}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
